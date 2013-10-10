2f579ac6b7fbbdb4edbcadc4818539dcae90c794
John Jeffery <jjeffery@sp.com.au>
2013-10-10 19:18:39 +1000

    Added Verify.ArgumentNotNull, nuget pkg restore

---
+++ src/Cesto.Tests/VerifyTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Cesto.Tests
+{
+	[TestFixture]
+	public class VerifyTests
+	{
+		[Test]
+		public void ArgumentNotNull_with_null_parameter()
+		{
+			string s = null;
+
+			try
+			{
+				Verify.ArgumentNotNull(s, "s");
+				Assert.Fail("Expected ArgumentNullException");
+			}
+			catch (ArgumentNullException ex)
+			{
+				Assert.AreEqual("s", ex.ParamName);
+			}
+		}
+
+		[Test]
+		public void ArgumentNotNull_with_non_null_parameter()
+		{
+			Assert.AreEqual("xyz", Verify.ArgumentNotNull("xyz", "s"));
+		}
+	}
+}
