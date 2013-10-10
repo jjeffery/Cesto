92d2fc4bdfdf3cc1302252891e69aa0f3376c05a
John Jeffery <jjeffery@sp.com.au>
2013-10-10 19:24:41 +1000

    Added DisposableAction class.

---
+++ src/Cesto.Tests/DisposableActionTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Cesto.Tests
+{
+	[TestFixture]
+	public class DisposableActionTests
+	{
+		public void Null_action()
+		{
+			// This is a pretty lame test -- really just verifies that
+			// nothing happens. No NullReferenceException, for example
+			using (new DisposableAction(null))
+			{
+			}
+		}
+
+		[Test]
+		public void Non_null_action()
+		{
+			bool didSomething = false;
+			var action = new Action(() => didSomething = true);
+			using (new DisposableAction(action))
+			{
+				Assert.IsFalse(didSomething);
+			}
+
+			Assert.IsTrue(didSomething);
+		}
+	}
+}
---
+++ src/Cesto/DisposableAction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Cesto
+{
+	/// <summary>
+	///     Class that performs a specified action when it is disposed.
+	/// </summary>
+	/// <remarks>
+	///     Useful for implementing 'using' patterns: where a cleanup action is performed
+	///     at the end of a using statement.
+	/// </remarks>
+	public class DisposableAction : IDisposable
+	{
+		private readonly Action _action;
+
+		/// <summary>
+		///     Create an object that implements <see cref="IDisposable" />, and
+		///     will perform a specific action when disposed.
+		/// </summary>
+		/// <param name="action">
+		///     The action that will be performed when <see cref="Dispose" /> is called.
+		/// </param>
+		public DisposableAction(Action action)
+		{
+			_action = action;
+		}
+
+		/// <summary>
+		///     <see cref="IDisposable.Dispose" />
+		/// </summary>
+		public void Dispose()
+		{
+			if (_action != null)
+			{
+				_action();
+			}
+		}
+	}
+}
