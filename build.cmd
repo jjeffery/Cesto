7aff98c641a52e814b1ae384927e005d0feaa4f3
John Jeffery <jjeffery@sp.com.au>
2013-10-16 19:05:40 +1000

    Work on formal build scripts. Using MSBuild.

---
+++ build.cmd
@@ -0,0 +1,2 @@
+msbuild build.proj /target:clean
+msbuild build.proj %1 %2 %3 %4
--- src/Cesto/Properties/AssemblyInfo.cs
+++ src/Cesto/Properties/AssemblyInfo.cs
@@ -6,11 +6,10 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 [assembly: AssemblyTitle("Cesto")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("Useful .NET Utility Classes")]
 [assembly: AssemblyProduct("Cesto")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright John Jeffery. All rights reserved.")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
