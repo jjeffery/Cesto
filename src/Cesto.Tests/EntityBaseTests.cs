2223ed32693ea154e383608b628e9fa998ad6cb2
John Jeffery <jjeffery@sp.com.au>
2015-03-09 14:09:42 +1000

    EntityBase

---
+++ src/Cesto.Tests/EntityBaseTests.cs
@@ -0,0 +1,275 @@
+﻿#region License
+
+// Copyright 2004-2014 John Jeffery
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using NUnit.Framework;
+
+// ReSharper disable InconsistentNaming
+namespace Cesto.Tests
+{
+	[TestFixture]
+	public class EntityBaseTests
+	{
+		[Test]
+		public void Int32Tests()
+		{
+			var id1A = new Int32Entity(1);
+			var id1B = new Int32Entity(1);
+			Assert.AreEqual(id1A, id1B);
+			Assert.IsFalse(id1A != id1B);
+			Assert.IsTrue(id1A == id1B);
+
+			// default values mean entities are not equal
+			var id0A = new Int32Entity(0);
+			var id0B = new Int32Entity(0);
+			Assert.AreNotEqual(id0A, id0B);
+			Assert.IsTrue(id0A != id0B);
+			Assert.IsFalse(id0A == id0B);
+
+			var id2 = new Int32Entity(2);
+			Assert.AreNotEqual(id1A, id2);
+			Assert.IsTrue(id1A != id2);
+			Assert.IsFalse(id1A == id2);
+
+			Assert.That(id0A.CompareTo(id1A), Is.LessThan(0));
+			Assert.That(id1A.CompareTo(id2), Is.LessThan(0));
+			Assert.That(id2.CompareTo(id1B), Is.GreaterThan(0));
+		}
+
+		[Test]
+		public void Int64Tests()
+		{
+			var id1A = new Int64Entity(1);
+			var id1B = new Int64Entity(1);
+			Assert.AreEqual(id1A, id1B);
+			Assert.IsFalse(id1A != id1B);
+			Assert.IsTrue(id1A == id1B);
+
+			// default values mean entities are not equal
+			var id0A = new Int64Entity(0);
+			var id0B = new Int64Entity(0);
+			Assert.AreNotEqual(id0A, id0B);
+			Assert.IsTrue(id0A != id0B);
+			Assert.IsFalse(id0A == id0B);
+
+			var id2 = new Int64Entity(2);
+			Assert.AreNotEqual(id1A, id2);
+			Assert.IsTrue(id1A != id2);
+			Assert.IsFalse(id1A == id2);
+
+			Assert.That(id0A.CompareTo(id1A), Is.LessThan(0));
+			Assert.That(id1A.CompareTo(id2), Is.LessThan(0));
+			Assert.That(id2.CompareTo(id1B), Is.GreaterThan(0));
+		}
+
+		[Test]
+		public void ValueTypeTests()
+		{
+			var id1A = new EnumEntity(EnumType.Enum1);
+			var id1B = new EnumEntity(EnumType.Enum1);
+			Assert.AreEqual(id1A, id1B);
+			Assert.IsFalse(id1A != id1B);
+			Assert.IsTrue(id1A == id1B);
+
+			// default values mean entities are not equal
+			var id0A = new EnumEntity(default(EnumType));
+			var id0B = new EnumEntity(default(EnumType));
+			Assert.AreNotEqual(id0A, id0B);
+			Assert.IsTrue(id0A != id0B);
+			Assert.IsFalse(id0A == id0B);
+
+			var id2 = new EnumEntity(EnumType.Enum2);
+			Assert.AreNotEqual(id1A, id2);
+			Assert.IsTrue(id1A != id2);
+			Assert.IsFalse(id1A == id2);
+
+			Assert.That(id0A.CompareTo(id1A), Is.LessThan(0));
+			Assert.That(id1A.CompareTo(id2), Is.LessThan(0));
+			Assert.That(id2.CompareTo(id1B), Is.GreaterThan(0));
+		}
+
+		[Test]
+		public void GuidTests()
+		{
+			var guid1 = Guid.NewGuid();
+			var guid2 = Guid.NewGuid();
+
+			// We want guid1 < guid2
+			if (guid1.CompareTo(guid2) > 0)
+			{
+				var temp = guid1;
+				guid1 = guid2;
+				guid2 = temp;
+			}
+			Assert.IsTrue(guid1.CompareTo(guid2) < 0);
+
+			var id1A = new GuidEntity(guid1);
+			var id1B = new GuidEntity(guid1);
+			Assert.AreEqual(id1A, id1B);
+			Assert.IsFalse(id1A != id1B);
+			Assert.IsTrue(id1A == id1B);
+
+			// default values mean entities are not equal
+			var id0A = new GuidEntity(default(Guid));
+			var id0B = new GuidEntity(default(Guid));
+			Assert.AreNotEqual(id0A, id0B);
+			Assert.IsTrue(id0A != id0B);
+			Assert.IsFalse(id0A == id0B);
+
+			var id2 = new GuidEntity(guid2);
+			Assert.AreNotEqual(id1A, id2);
+			Assert.IsTrue(id1A != id2);
+			Assert.IsFalse(id1A == id2);
+
+			Assert.That(id0A.CompareTo(id1A), Is.LessThan(0));
+			Assert.That(id1A.CompareTo(id2), Is.LessThan(0));
+			Assert.That(id2.CompareTo(id1B), Is.GreaterThan(0));
+		}
+
+		[Test]
+		public void StringTests()
+		{
+			var id1A = new StringEntity("XX");
+			var id1B = new StringEntity("xx");
+			Assert.AreEqual(id1A, id1B);
+			Assert.IsFalse(id1A != id1B);
+			Assert.IsTrue(id1A == id1B);
+
+			// default values mean entities are not equal
+			var id0A = new StringEntity(null);
+			var id0B = new StringEntity(null);
+			Assert.AreNotEqual(id0A, id0B);
+			Assert.IsTrue(id0A != id0B);
+			Assert.IsFalse(id0A == id0B);
+
+			var id2 = new StringEntity("ZZ");
+			Assert.AreNotEqual(id1A, id2);
+			Assert.IsTrue(id1A != id2);
+			Assert.IsFalse(id1A == id2);
+
+			Assert.That(id0A.CompareTo(id1A), Is.LessThan(0));
+			Assert.That(id1A.CompareTo(id2), Is.LessThan(0));
+			Assert.That(id2.CompareTo(id1B), Is.GreaterThan(0));
+		}
+
+		[Test]
+		public void GetHashCode_returns_same_value_if_id_changes()
+		{
+			var entity = new EntityWhoseIdCanChange {Id = 1};
+
+			var hashCode1 = entity.GetHashCode();
+			var hashCode2 = entity.GetHashCode();
+
+			Assert.AreEqual(hashCode1, hashCode2);
+
+			entity.Id = 2;
+		    Assert.AreEqual(hashCode1, entity.GetHashCode());
+		}
+
+		public class EntityWhoseIdCanChange : EntityBase<EntityWhoseIdCanChange, int>
+		{
+			public int Id;
+
+			protected override int GetId()
+			{
+				return Id;
+			}
+		}
+
+		public class Int32Entity : EntityBase<Int32Entity, int>
+		{
+			public readonly int Id;
+
+			public Int32Entity(int id)
+			{
+				Id = id;
+			}
+
+			protected override int GetId()
+			{
+				return Id;
+			}
+		}
+
+		public class Int64Entity : EntityBase<Int64Entity, long>
+		{
+			public readonly long Id;
+
+			public Int64Entity(long id)
+			{
+				Id = id;
+			}
+
+			protected override long GetId()
+			{
+				return Id;
+			}
+		}
+
+		public class StringEntity : EntityBase<StringEntity, string>
+		{
+			public readonly string Id;
+
+			public StringEntity(string id)
+			{
+				Id = id;
+			}
+
+			protected override string GetId()
+			{
+				return Id;
+			}
+		}
+
+		public enum EnumType
+		{
+			Enum1 = 1,
+			Enum2 = 2,
+		}
+
+		public class EnumEntity : EntityBase<EnumEntity, EnumType>
+		{
+			public readonly EnumType Id;
+
+			public EnumEntity(EnumType id)
+			{
+				Id = id;
+			}
+
+			protected override EnumType GetId()
+			{
+				return Id;
+			}
+		}
+
+		public class GuidEntity : EntityBase<GuidEntity, Guid>
+		{
+			public readonly Guid Id;
+
+			public GuidEntity(Guid id)
+			{
+				Id = id;
+			}
+
+			protected override Guid GetId()
+			{
+				return Id;
+			}
+		}
+	}
+}
---
+++ src/Cesto.Tests/IdHelperTests.cs
@@ -0,0 +1,155 @@
+﻿#region License
+
+// Copyright 2004-2014 John Jeffery
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using Cesto.Internal;
+using NUnit.Framework;
+
+// ReSharper disable InconsistentNaming
+namespace Cesto.Tests
+{
+	[TestFixture]
+	public class IdHelperTests
+	{
+		[Test]
+		public void Value_type_short()
+		{
+			var helper = new IdHelperForValueType<short>();
+
+			Assert.AreEqual(false, helper.IsNull(0));
+			Assert.AreEqual(true, helper.IsDefaultValue(0));
+			Assert.AreEqual(false, helper.IsDefaultValue(1));
+			Assert.AreEqual(true, helper.AreEqual(0, 0));
+			Assert.AreEqual(true, helper.AreEqual(23, 23));
+			Assert.AreEqual(-1, helper.Compare(0, 1));
+			Assert.AreEqual(+1, helper.Compare(0, -1));
+			Assert.That(helper.Compare(0, 0), Is.EqualTo(0));
+			Assert.AreEqual(0, helper.Compare(23, 23));
+			Assert.AreEqual(0.GetHashCode(), helper.GetHashCode(0));
+			Assert.AreEqual(((short)23).GetHashCode(), helper.GetHashCode(23));
+		}
+
+		[Test]
+		public void Value_type_long()
+		{
+			var helper = new IdHelperForInt64();
+
+			Assert.AreEqual(false, helper.IsNull(0));
+			Assert.AreEqual(true, helper.IsDefaultValue(0));
+			Assert.AreEqual(false, helper.IsDefaultValue(1));
+			Assert.AreEqual(true, helper.AreEqual(0, 0));
+			Assert.AreEqual(true, helper.AreEqual(23, 23));
+			Assert.AreEqual(-1, helper.Compare(0, 1));
+			Assert.AreEqual(+1, helper.Compare(0, -1));
+			Assert.That(helper.Compare(0, 0), Is.EqualTo(0));
+			Assert.AreEqual(0, helper.Compare(23, 23));
+			Assert.AreEqual(0.GetHashCode(), helper.GetHashCode(0));
+			Assert.AreEqual(((long)23).GetHashCode(), helper.GetHashCode(23));
+		}
+
+
+		[Test]
+		public void Value_type_int()
+		{
+			var helper = new IdHelperForInt32();
+
+			Assert.AreEqual(false, helper.IsNull(0));
+			Assert.AreEqual(true, helper.IsDefaultValue(0));
+			Assert.AreEqual(false, helper.IsDefaultValue(1));
+			Assert.AreEqual(true, helper.AreEqual(0, 0));
+			Assert.AreEqual(true, helper.AreEqual(23, 23));
+			Assert.AreEqual(-1, helper.Compare(0, 1));
+			Assert.AreEqual(+1, helper.Compare(0, -1));
+			Assert.That(helper.Compare(0, 0), Is.EqualTo(0));
+			Assert.AreEqual(0, helper.Compare(23, 23));
+			Assert.AreEqual(0.GetHashCode(), helper.GetHashCode(0));
+			Assert.AreEqual(23.GetHashCode(), helper.GetHashCode(23));
+		}
+
+		[Test]
+		public void Value_type_Guid()
+		{
+			var helper = new IdHelperForValueType<Guid>();
+
+			var guid1 = new Guid("005faa88-a529-4065-9d04-72a96316988b");
+			var guid2 = new Guid("64b9dd7a-f46e-448b-b6e5-ddd3f83ec508");
+
+			Assert.AreEqual(false, helper.IsNull(Guid.Empty));
+			Assert.AreEqual(true, helper.IsDefaultValue(Guid.Empty));
+			Assert.AreEqual(false, helper.IsDefaultValue(guid1));
+			Assert.AreEqual(true, helper.AreEqual(Guid.Empty, Guid.Empty));
+			Assert.AreEqual(true, helper.AreEqual(guid1, guid1));
+			Assert.AreEqual(-1, helper.Compare(Guid.Empty, guid1));
+			Assert.AreEqual(+1, helper.Compare(guid2, Guid.Empty));
+			Assert.That(helper.Compare(Guid.Empty, Guid.Empty), Is.EqualTo(0));
+			Assert.AreEqual(0, helper.Compare(guid2, guid2));
+			Assert.AreEqual(Guid.Empty.GetHashCode(), helper.GetHashCode(Guid.Empty));
+			Assert.AreEqual(guid1.GetHashCode(), helper.GetHashCode(guid1));
+		}
+
+		[Test]
+		public void Value_type_string()
+		{
+			var helper = new IdHelperForString();
+
+			Assert.AreEqual(true, helper.IsNull(null));
+			Assert.AreEqual(true, helper.IsDefaultValue(null));
+			Assert.AreEqual(false, helper.IsDefaultValue("XX"));
+			Assert.AreEqual(true, helper.AreEqual(null, null));
+			Assert.AreEqual(false, helper.AreEqual(null, "X"));
+			Assert.AreEqual(true, helper.AreEqual("ABCD", "abcd"));
+			Assert.AreEqual(true, helper.AreEqual("ABCDE", "ABCDE"));
+			Assert.AreEqual(-1, helper.Compare(null, "AAA"));
+			Assert.AreEqual(+1, helper.Compare("BBB", null));
+			Assert.That(helper.Compare(null, null), Is.EqualTo(0));
+			Assert.AreEqual(0, helper.Compare("X", "x"));
+			Assert.AreEqual(string.Empty.GetHashCode(), helper.GetHashCode(string.Empty));
+			Assert.AreEqual(0, helper.GetHashCode(null));
+			Assert.AreEqual("XXX".GetHashCode(), helper.GetHashCode("XXX"));
+			Assert.AreEqual(helper.GetHashCode("XXX"), helper.GetHashCode("xxx"));
+		}
+
+#if NET40
+		[Test]
+		public void Value_type_tuple()
+		{
+			// This is a bit contrived, just need an easy class (ie non-value type)
+			var helper = new IdHelperForClassType<Tuple<int, int>>();
+
+			var tuple1 = new Tuple<int, int>(1, 2);
+			var tuple2 = new Tuple<int, int>(1, 3);
+
+			var tuple1Again = new Tuple<int, int>(1, 2);
+
+			Assert.AreEqual(true, helper.IsNull(null));
+			Assert.AreEqual(true, helper.IsDefaultValue(null));
+			Assert.AreEqual(false, helper.IsDefaultValue(tuple1));
+			Assert.AreEqual(true, helper.AreEqual(null, null));
+			Assert.AreEqual(true, helper.AreEqual(tuple1, tuple1Again));
+			Assert.AreEqual(-1, helper.Compare(null, tuple1));
+			Assert.AreEqual(-1, helper.Compare(tuple1, tuple2));
+			Assert.AreEqual(+1, helper.Compare(tuple2, null));
+			Assert.AreEqual(+1, helper.Compare(tuple2, tuple1));
+			Assert.That(helper.Compare(null, null), Is.EqualTo(0));
+			Assert.AreEqual(0, helper.Compare(tuple1, tuple1Again));
+			Assert.AreEqual(tuple1.GetHashCode(), helper.GetHashCode(tuple1));
+			Assert.AreEqual(0, helper.GetHashCode(null));
+		}
+#endif
+	}
+}
