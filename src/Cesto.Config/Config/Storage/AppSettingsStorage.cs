0d21bfc45843f5fa3df9b794b59c3c1a8c110da5
John Jeffery <jjeffery@sp.com.au>
2015-04-19 10:49:44 +1000

    Added AppSettingsStorage

---
+++ src/Cesto.Config/Config/Storage/AppSettingsStorage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Cesto.Config.Storage
+{
+	public class AppSettingsStorage : IConfigStorage
+	{
+		public ConfigValue GetValue(ConfigParameter parameter)
+		{
+			var setting = ConfigurationManager.AppSettings[parameter.Name];
+			if (setting == null)
+			{
+				return new ConfigValue(parameter, null, false);
+			}
+			return new ConfigValue(parameter, setting, true);
+		}
+
+		public ConfigValue[] GetValues(params ConfigParameter[] parameters)
+		{
+			var values = new ConfigValue[parameters.Length];
+			for (var index = 0; index < parameters.Length; ++index)
+			{
+				values[index] = GetValue(parameters[index]);
+			}
+			return values;
+		}
+
+		public bool IsReadOnly
+		{
+			get { return true; }
+		}
+
+		public void SetValue(ConfigParameter parameter, string value)
+		{
+			throw new NotSupportedException();
+		}
+
+		public void Refresh()
+		{
+		}
+	}
+}
