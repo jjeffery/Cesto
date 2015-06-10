6c73d6d332e917aef95ade08b8d328f4f4bd37a0
John Jeffery <jjeffery@sp.com.au>
2015-06-10 14:43:10 +1000

    Add SyncTimer to Cesto.Core.
    This is the first step of removing the Cesto.Threading assembly.

---
+++ src/Cesto.Core/Threading/SyncTimer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Threading;
+
+namespace Cesto.Threading
+{
+	/// <summary>
+	/// A one-shot timer that performs an action when the timer expires.
+	/// The action will be performed by posting to the SynchronizationContext.
+	/// This can be used to ensure that the timer action is called on a particular thread.
+	/// </summary>
+	public class SyncTimer : IDisposable
+	{
+		// Action called when the timer expires
+		private readonly Action _action;
+
+		// Timer that does the work
+		private readonly Timer _timer;
+
+		/// <summary>
+		/// The <see cref="SynchronizationContext"/> associated with this <see cref="SyncTimer"/>
+		/// </summary>
+		public readonly SynchronizationContext SynchronizationContext;
+
+		/// <summary>
+		/// Constructs a SyncTimer.
+		/// </summary>
+		/// <param name="action">
+		/// The action to perform whenever the timer expires.
+		/// </param>
+		/// <param name="synchronizationContext">
+		/// The synchronization context to use to execute the action when the timer expires.
+		/// If not specified, the current synchronization context is used.
+		/// </param>
+		public SyncTimer(Action action, SynchronizationContext synchronizationContext = null)
+		{
+			_timer = new Timer(TimerCallback);
+			_action = Verify.ArgumentNotNull(action, "action");
+			if (synchronizationContext == null)
+			{
+				synchronizationContext = SynchronizationContext.Current;
+				if (synchronizationContext == null)
+				{
+					throw new InvalidOperationException("No synchronization context specified, and no current synchronization context exists");
+				}
+				SynchronizationContext = synchronizationContext;
+			}
+		}
+
+		// Called when the timer expires.
+		private void TimerCallback(object state)
+		{
+			SynchronizationContext.Post(SynchronizationContextCallback, null);
+		}
+
+		// Called by the timer callback via the synchronization context
+		private void SynchronizationContextCallback(object state)
+		{
+			// This method is called on the correct thread to call the action
+			_action();
+		}
+
+		public void Dispose()
+		{
+			Stop();
+			_timer.Dispose();
+		}
+
+		/// <summary>
+		/// Starts the timer. If the timer was already running, then the timer is restarted.
+		/// </summary>
+		/// <param name="timeout">Timeout</param>
+		public void Start(TimeSpan timeout)
+		{
+			Stop();
+			_timer.Change(timeout, TimeSpan.FromMilliseconds(-1));
+		}
+
+		/// <summary>
+		/// Starts the timer. If the timer was already running, then the timer is restarted.
+		/// </summary>
+		/// <param name="milliseconds">Timeout in milliseconds</param>
+		public void Start(int milliseconds)
+		{
+			Stop();
+			_timer.Change(milliseconds, Timeout.Infinite);
+		}
+
+		/// <summary>
+		/// Stops the timer. The timer can be started again.
+		/// </summary>
+		public void Stop()
+		{
+			_timer.Change(Timeout.Infinite, Timeout.Infinite);
+		}
+	}
+}
---
+++ src/Cesto.Tests/Threading/SyncTimerTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Cesto.Threading;
+using NUnit.Framework;
+
+namespace Cesto.Tests.Threading
+{
+	[TestFixture]
+	public class SyncTimerTests
+	{
+		private FakeSynchronizationContext _fakeSynchronizationContext;
+		private SynchronizationContext _previousSynchronizationContext;
+
+		[SetUp]
+		public void Setup()
+		{
+			_previousSynchronizationContext = SynchronizationContext.Current;
+			_fakeSynchronizationContext = new FakeSynchronizationContext();
+			SynchronizationContext.SetSynchronizationContext(_fakeSynchronizationContext);
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			SynchronizationContext.SetSynchronizationContext(_previousSynchronizationContext);
+		}
+
+		[Test]
+		public void Throws_if_no_SynchronizationContext()
+		{
+			SynchronizationContext.SetSynchronizationContext(null);
+			// ReSharper disable once ObjectCreationAsStatement
+			Assert.Throws<InvalidOperationException>(() => new SyncTimer(() => { }));
+		}
+
+		[Test]
+		public async Task StartWithTimeSpan()
+		{
+			var timerExpired = false;
+			SynchronizationContext sc = null;
+			var syncTimer = new SyncTimer(() => {
+				timerExpired = true;
+				sc = SynchronizationContext.Current;
+			});
+			syncTimer.Start(TimeSpan.FromMilliseconds(50));
+			await Task.Delay(120);
+			Assert.AreEqual(true, timerExpired);
+			Assert.AreSame(_fakeSynchronizationContext, sc);
+		}
+
+		[Test]
+		public async Task StartWithInt32()
+		{
+			var timerExpired = false;
+			SynchronizationContext sc = null;
+			var syncTimer = new SyncTimer(() => {
+				timerExpired = true;
+				sc = SynchronizationContext.Current;
+			});
+			syncTimer.Start(50);
+			await Task.Delay(120);
+			Assert.AreEqual(true, timerExpired);
+			Assert.AreSame(_fakeSynchronizationContext, sc);
+		}
+
+		[Test]
+		public async Task Stop()
+		{
+			var timerExpired = false;
+			var syncTimer = new SyncTimer(() => {
+				timerExpired = true;
+			});
+			syncTimer.Start(120);
+			await Task.Delay(50);
+			syncTimer.Stop();
+			await Task.Delay(120);
+			Assert.AreEqual(false, timerExpired);
+		}
+
+		[Test]
+		public async Task StopsWhenDisposed()
+		{
+			var timerExpired = false;
+			var syncTimer = new SyncTimer(() => {
+				timerExpired = true;
+			});
+			syncTimer.Start(120);
+			await Task.Delay(50);
+			syncTimer.Dispose();
+			await Task.Delay(120);
+			Assert.AreEqual(false, timerExpired);
+		}
+
+
+		public class FakeSynchronizationContext : SynchronizationContext
+		{
+			public override void Post(SendOrPostCallback d, object state)
+			{
+				SendOrPostCallback callback = s => {
+					var sc = Current;
+					SetSynchronizationContext(this);
+					try
+					{
+						d(state);
+					}
+					finally
+					{
+						SetSynchronizationContext(sc);
+					}
+				};
+
+				base.Post(callback, state);
+			}
+		}
+	}
+}
